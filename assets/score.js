6e7945aaec31213871baf3c5aa35ca4a390b35ef
Robert Vunabandi <rvunabandi@gmail.com>
2018-02-17 01:56:53 -0500

    no ajax

--- assets/score.js
+++ assets/score.js
@@ -1,53 +1,3 @@
-function populateAverageScore() {
-	// endpoint: GET average
-	$.ajax({
-		type: 'GET',
-		url: '/average',
-		success: function(average_score) {
-			// populate the average on the web page
-			document.getElementById('average').innerHMTL = average_score;
-		},
-		error: function(xhr, status, error) {
-			handleError(error);
-		}
-	});
-}
-function populateMaxScore() {
-	// endpoint: GET max
-		$.ajax({
-		type: 'GET',
-		url: '/max',
-		success: function(maximum_score) {
-			// populate the max score on the web page
-			document.getElementById('maximum').innerHMTL = maximum_score;
-		},
-		error: function(xhr, status, error) {
-			handleError(error);
-		}
-	});
-}
-
-function postScore(score, callback) {
-	// endpoint: POST add, PARAM: score
-	let callbackHandler = callback[0];
-	let handleError = callback[1];
-	$.ajax({
-		type: 'POST',
-		url: '/add',
-		data: { score: score },
-		success: function() {
-			populateAverageScore();
-			populateMaxScore();
-			if (callback) {
-				callbackHandler();
-			}
-		},
-		error: function(xhr, status, error) {
-			handleError(score, error);
-		}
-	});
-}
-
 /**
  * 
  * DO NOT MODIFY THE CODE BELOW
@@ -68,23 +18,8 @@ function main() {
 	score = window.location.href.substring(index);
 	score = parseInt(score.replace(/\#/, ''), 16);
 	document.getElementById('outof').style.display = 'none';
-	try {
-		postScore(score, [function() {
-					populateScore(score);
-				}, function(score, err) {
-					postScoreErrorHandler(score, err);
-				}]);
-	} catch (err) {
-		postScoreErrorHandler(score, err);
-	}
-};
-
-function postScoreErrorHandler(score, err) {
-	console.log('an error occurred while posting the score');
-	console.error(err);
-	// populate the score anyway
 	populateScore(score);
-}
+};
 
 function populateScore(score) {
 	let count = 4;
